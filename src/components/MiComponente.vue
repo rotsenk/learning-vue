bd4f9e36d1ba9c65048dc2b0d721785aa6a0bf02
rotsenk <stanley.95@hotmail.com>
2023-11-28 09:44:07 -0600

    MiComponente Created

---
+++ src/components/MiComponente.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="mi-componente">
+    <hr />
+    <h1>Este es MI COMPONENTE</h1>
+    <h2>{{ subtitulo }}</h2>
+    <hr />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MiComponente",
+  data() {
+    return {
+      subtitulo: "Soy el subtitulo de mi componente",
+    };
+  },
+};
+</script>
