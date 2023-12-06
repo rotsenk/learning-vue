bfa4a360b8a02f999f3dd31d5790d5d9372f0953
rotsenk <stanley.95@hotmail.com>
2023-12-05 19:34:56 -0600

    Others components created

---
+++ src/components/PaginaComponent.vue
@@ -0,0 +1,11 @@
+<template>
+  <section id="content">
+    <h2 class="subheader">PÁGINA DE PRUEBA</h2>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "PaginaComponent",
+};
+</script>
